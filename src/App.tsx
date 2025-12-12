01515ff63db21a80e5cadb151b20462232a6b61f
sina-byn <sina.sina.bayan@protonmail.com>
2025-12-12 12:05:40 +0330

    chore: sample usage added for testing purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,36 @@
+import { createAuthHooks } from '@/lib';
+
+const { useLogin } = createAuthHooks({
+  login: async (username: string) => {
+    const resp = await fetch('http://localhost:3000/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+      }),
+    });
+
+    const data = await resp.json();
+    return data;
+  },
+  signup: async () => {},
+  logout: async () => {},
+  user: async () => {},
+});
+
 export function App() {
-  return <div>better-react-query-auth</div>;
+  const mutation = useLogin();
+
+  return (
+    <div>
+      <button
+        type='button'
+        onClick={async () => {
+          const data = await mutation.mutateAsync('username');
+          console.log(data);
+        }}
+      >
+        login
+      </button>
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from './App.tsx';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
 );
